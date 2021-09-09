883a27265c3fc3c387893bc73127d63c89717f00
Łukasz <lsliwaradioluz@gmail.com>
2021-09-09 17:29:56 +0200

    Create the default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <app-header />
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader.vue';
+export default {
+  components: { AppHeader }
+}
+</script>
+
+<style>
+
+</style>
